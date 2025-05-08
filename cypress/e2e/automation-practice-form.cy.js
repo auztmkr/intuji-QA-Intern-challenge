c8d9d306d650621a0fcf7f04c02a8cbcc4799d58
Ayus Tamrakar <142563909+auztmkr@users.noreply.github.com>
2025-05-08 10:23:23 +0545

    Update automation-practice-form.cy.js
    test: fill out and successfully submit the automation practice form.

--- cypress/e2e/automation-practice-form.cy.js
+++ cypress/e2e/automation-practice-form.cy.js
@@ -9,3 +9,47 @@ describe('Automation Practice Form', () => {
     // Visit the form before each test
     cy.visit('https://demoqa.com/automation-practice-form');
   });
+
+t('Should fill the form and submit successfully', () => {
+    // Fill Name
+    cy.get('#firstName').type('Ayus');
+    cy.get('#lastName').type('Tamrakar');
+    
+    // Fill Email
+    cy.get('#userEmail').type('ayust@test.com');
+    
+    // Select Gender (Male)
+    cy.get('label[for="gender-radio-1"]').click();
+    
+    // Fill Mobile Number
+    cy.get('#userNumber').type('9876543210');
+    
+    // Select Date of Birth
+    cy.get('#dateOfBirthInput').click();
+    cy.get('.react-datepicker__month-select').select('April');
+    cy.get('.react-datepicker__year-select').select('1995');
+    cy.get('.react-datepicker__day--015:not(.react-datepicker__day--outside-month)').click();
+
+    // Fill Subjects
+    cy.get('#subjectsInput').type('Maths{enter}');
+    
+    // Select Hobbies
+    cy.get('label[for="hobbies-checkbox-1"]').click(); // Sports
+    cy.get('label[for="hobbies-checkbox-2"]').click(); // Reading
+    
+    // Upload Picture
+    cy.get('input[type="file"]').attachFile('example.png');
+
+    // Fill Address
+    cy.get('#currentAddress').type('NewRoad, Kathmandu');
+
+    // Select State and City
+    cy.get('#state').click().find('.css-26l3qy-menu').contains('NCR').click();
+    cy.get('#city').click().find('.css-26l3qy-menu').contains('Delhi').click();
+
+    // Submit Form
+    cy.get('#submit').click();
+
+    // Assert the submission modal
+    cy.get('.modal-content').should('contain', 'Thanks for submitting the form');
+  });
